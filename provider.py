61e6af1563a005a7179b63cd5bd2e99ddf7d9587
Nasef M.Aly <154965352+Nasef2017@users.noreply.github.com>
2025-09-21 17:12:07 +0300

    Update provider.py

--- provider.py
+++ provider.py
@@ -1,39 +1,61 @@
 from qgis.core import QgsProcessingProvider, QgsProcessingAlgorithm
-import os, inspect, importlib.util
-
-ALG_DIR = os.path.join(os.path.dirname(__file__), 'algorithms')
+import os
+import inspect
+import importlib.util
+import sys
 
 class SdbProvider(QgsProcessingProvider):
+
     def __init__(self):
+        """Constructor."""
         super().__init__()
         self._algorithms = self._load_algorithms()
 
-    def loadAlgorithms(self):
+    def loadAlgorithms(self, *args, **kwargs):
+        """Loads all processing algorithms from this provider."""
         for alg in self._algorithms:
             self.addAlgorithm(alg)
             
     def unload(self):
+        """Unloads the provider."""
         super().unload()
         self._algorithms = []
 
-    def id(self): return 'sdb_tools'
-    def name(self): return 'SDB Tools'
-    def longName(self): return self.name()
+    def id(self):
+        """Returns the unique provider id."""
+        return 'sdb_tools'
+
+    def name(self):
+        """Returns the provider name."""
+        return 'SDB Tools'
+
+    def longName(self):
+        """Returns the a more verbose provider name."""
+        return self.name()
 
     def _load_algorithms(self):
+        """Dynamically loads all algorithm classes from .py files."""
         algorithms = []
-        if not os.path.isdir(ALG_DIR): return []
-        for file_name in os.listdir(ALG_DIR):
-            if file_name.endswith('.py') and file_name != '__init__.py':
-                file_path = os.path.join(ALG_DIR, file_name)
-                module_name = f"sdb_tools.algorithms.{file_name[:-3]}"
+        alg_folder = os.path.join(os.path.dirname(__file__), 'algorithms')
+        if not os.path.isdir(alg_folder):
+            return []
+
+        for f in os.scandir(alg_folder):
+            if f.is_file() and f.name.endswith('.py') and f.name != '__init__.py':
+                module_name = f'sdb_tools.algorithms.{f.name[:-3]}'
+                
+                # In case the plugin was reloaded, remove the old module
+                if module_name in sys.modules:
+                    del sys.modules[module_name]
+
                 try:
-                    spec = importlib.util.spec_from_file_location(module_name, file_path)
+                    spec = importlib.util.spec_from_file_location(module_name, f.path)
                     module = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(module)
+                    
                     for name, obj in inspect.getmembers(module, inspect.isclass):
                         if issubclass(obj, QgsProcessingAlgorithm) and obj is not QgsProcessingAlgorithm:
                             algorithms.append(obj())
                 except Exception as e:
-                    print(f"SDB Tools: Error loading {file_name}: {e}")
+                    print(f"SDB Tools: Error loading algorithm from {f.name}: {e}")
         return algorithms
